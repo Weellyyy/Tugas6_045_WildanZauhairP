724ee09de7f22ef9ff1907bd620fa043b8ba7ca1
Weellyyy <wildanprtm2304@gmail.com>
2025-04-28 06:43:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.getElementById("video");
+const fotoElement = document.getElementById("foto");
+const audioElement = document.getElementById("audio");
+
+videoElement.addEventListener("click", () => {
+    alert("Hiburan sejenakkk");
+});
+
+fotoElement.addEventListener("click", () => {
+    alert("Mengandung Fakta dekkk");
+});
+
+audioElement.addEventListener("click", () => {
+    alert("Biasakan nonton Madrid sampe akhir culll");
+});
